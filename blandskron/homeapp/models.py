59e5722fd093a767ea5f62cbe5cbaad8b1b7a55d
blandskron <bastianlandskronfreelancer@gmail.com>
2024-06-14 01:34:21 -0400

    avanzando

--- blandskron/homeapp/models.py
+++ blandskron/homeapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class HardSkill(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='skill_images/', blank=True, null=True)
+    project_url = models.URLField()
+
+    def __str__(self):
+        return self.name
--- blandskron/homeapp/views.py
+++ blandskron/homeapp/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import HardSkill
+
 
 def home(request):
     return render(request, 'homeapp/home.html')
+
+def hardskills(request):
+    skills = HardSkill.objects.all()
+    return render(request, 'homeapp/hardskills.html', {'skills': skills})
